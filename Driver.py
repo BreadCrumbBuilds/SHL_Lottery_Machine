8da80308f0381c4e9f101cf6083d7d725b041997
Brad Crump <bradrcrump@gmail.com>
2023-10-05 13:16:33 -0700

    remove unused files

--- Driver.py
+++ Driver.py
@@ -1,195 +1,135 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 12 19:01:48 2019
+import json
+import random
+import heapq
+import time as t
 
-@author: BradleyCrump
+def nth_highest_seed(cohort, n):
+    largest_seeds = heapq.nlargest(n, cohort.values())
+    nth_highest_seed = largest_seeds[-1]
 
-Finished:
-	Read and write the results.
-TODO:
-	Read back the results in reverse order.
-	Make firebase web application
-"""
-import Lottery as lotto
-import Util as util
-import DisplayResults as displayResults
-"""
-Section 1 - Make files to write to
-"""
-########################################################################
-# Files to read and write
-StandingsFile = open('./docs/TeamStandings.txt', 'r')
-LotteryStandingsFile = open('./docs/LotteryStandings.txt', 'w')
-ResultsLog = open('./docs/LotteryResults.txt', 'w')
-LottoTestLog = open('./docs/TestLog.txt', 'w')
-# End of section 1
-
-"""
-Section 2 - Make the Teams from the text file
-"""
-########################################################################
-
-# Lists to make
-League = []
-LeagueCopy = []
-LotteryResults = []
-
-# Make the League by pulling the text out of Standings.txt
-for line in StandingsFile:
-    # for is used to loop through something. In this case, we
-    # are looping through each line of the Standings file.
-    # Each line, we execute the following instructions
-    line = line.strip()                     # gets rid of new line characters
-    name, standing = line.split(":")        # divides the line into two by :
-    standing = standing.replace(" ", "")    # gets rid of extra spaces
-    team = [name, int(standing)]            # make a team
-    League.append(team)                     # put the team in the league
-
-LeagueCopy = League.copy() 		    # to be used when reading results back
-
-header = ("{0:^20}".format("Owner") + "{:<23}".format("Standing"))
-print("\n\n\nWelcome to the Selfish Hockey League Draft Lottery!!!")
-util.waitNoDot(3)
-print("\nThe Selfish Hockey League 2022 Standings")
-util.waitNoDot(3)
-print("-----------------------------------------")
-util.waitNoDot(.5)
-print(header)
-for team in League: 	# show the league
-    util.waitNoDot(1)
-    print('{:<20}'.format(team[0]), '{:^5}'.format(team[1]))
-
-# End of section 2
-
-# Section 3 - Test the Lottery
-
-########################################################################
-util.waitNoDot(10)
-print("\nLottery Testing 1,000,000 Draws, five times... Five million draws")
-
-LottoTestLog.write("\nLOTTERY TESTING 1,000,000 DRAWs\n")
-
-# Do five tests of the lottery
-
-i = 0
-while i < 5:
-    string = lotto.LotteryTest(1000000)
-    LottoTestLog.write(string)
-    print(string)
-    util.waitNoDot(3)
-    i += 1
-
-# End of section 3
-
-"""
-Section 4 - Do the lottery, save the results into a Log file
-to be read in later
-"""
-########################################################################
-
-count = 1
-
-# Do the lotto: 4 teams at a time, until there are 3 teams left
-for idx in range(len(League)):  # run until only three teams left
-    if len(League) < 4: break
-    # Initialize temporary list to store each round of lottery
-    LotteryPool = []
-
-    # Make and write header
-    header = "Round " + str(count) + "*\n"
-    ResultsLog.write(header)
-
-    # Get the first seed from the league
-    firstSeed = League[(len(League) - 1)]
-    firstString = "1- " + firstSeed[0] + ": " + str(firstSeed[1]) + "\n"
-
-    ResultsLog.write(firstString)
-
-    # Check if this team has shit the bed on every lottery so far
-    # We do this by comparing the size of the league (how many teams
-    # are left) to the standing of that team.
-    # The size of the league shrinks by one with every pass through the lottery,
-    # so if the standing of team - 3 is ever larger than the amount of teams
-    # remaining, that team has fallen the maximum four spots, so they
-    # automatically win this round
-    print(f'seed {firstSeed[1]}, round {idx+1}, exception {(int)(firstSeed[1] + 3) <= idx+1}')
-    if (int)(firstSeed[1] + 3) <= idx+1:
-        # take out the team and put it in the Winner
-        LotteryResults.append(League.pop(len(League)-1))
-        firstStringLost = "L- " + \
-            firstSeed[0] + ": " + str(firstSeed[1]) + "\n"
-        ResultsLog.write(firstStringLost)
-        count += 1
-        continue  # if this was true, start the loop over
-
-    # otherwise get the rest of the teams and do the damn lottery
-
-    secondSeed = League[len(League) - 2]
-    secondString = "2- " + secondSeed[0] + ": " + str(secondSeed[1]) + "\n"
-    ResultsLog.write(secondString)
-
-    thirdSeed = League[len(League) - 3]
-    thirdString = "3- " + thirdSeed[0] + ": " + str(thirdSeed[1]) + "\n"
-    ResultsLog.write(thirdString)
-
-    fourthSeed = League[len(League) - 4]
-    fourthString = "4- " + fourthSeed[0] + ": " + str(fourthSeed[1]) + "\n"
-    ResultsLog.write(fourthString)
-
-    # Put all the teams together in the pool
-    LotteryPool = [firstSeed, secondSeed, thirdSeed, fourthSeed]
-
-    # Find winner
-    winner = lotto.Draw()
-
-    # Get the winning team
-    winningSeed = LotteryPool[winner]
-    winningString = "W- " + winningSeed[0] + ": " + str(winningSeed[1]) + "\n"
-    ResultsLog.write(winningString)
-
-    # Get the index of the winner in the League list (different list than the pool)
-    index = League.index(winningSeed)
-
-    # Take the winner out of the League list, and put him in the Results list
-    LotteryResults.append(League.pop(index))
-
-    count += 1
-
-# Three teams left
-while len(League) > 0:
-
-    # Take them out of the League list in order, put them in results
-    header = "Round " + str(count) + "*\n"
-    ResultsLog.write(header)
-
-    team = League.pop()
-    LotteryResults.append(team)
-
-    teamString = "L- " + team[0] + ": " + str(team[1]) + "\n"
-    ResultsLog.write(teamString)
-
-    count += 1
-
-count = 1
-
-LotteryStandingsFile.write("\n\nRESULTS\n---------\n")
-
-header = '{:^24}'.format("Team") + '{:>5}'.format("Seed\n")
-header += "---------------------------------------\n"
-LotteryStandingsFile.write(header)
-
-for team in LotteryResults:
-
-    LotteryStandingsFile.write('{:<4}'.format("#" + (str(count))) + '| ' +
-                               '{:<20}'.format(str(team[0])) + "| "
-                               '{:<5}'.format(str(team[1])) + "\n")
-    count += 1
+    return nth_highest_seed
 
 
-StandingsFile.close()
-ResultsLog.close()
-LotteryStandingsFile.close()
-LottoTestLog.close()
+def lottery():
+    numbers = [1, 2, 3, 4]
+    probabilities = [0.5, 0.25, 0.15, 0.1]
+    result = random.choices(numbers, weights=probabilities, k=1)[0]
 
-displayResults.run()
+    return result
+
+def get_highest_seed(data_dict):
+    seeds = [value for value in data_dict.values() if isinstance(value, int)]
+
+    if not seeds:
+        raise ValueError("No valid seeds found in the dictionary.")
+
+    highest_seed = max(seeds)
+
+    return highest_seed
+
+
+def get_teams_for_lottery_round(data_dict):
+    sorted_data = sorted(data_dict.items(), reverse=True, key=lambda x: x[1])
+    four_lowest = sorted_data[:4]
+    result = dict(four_lowest)
+
+    return result
+
+
+def draft_pick(data_dict, draft_round):
+    highest_seed = get_highest_seed(data_dict)
+    if draft_round < highest_seed - 2:
+        return 1
+    else:
+        return lottery()
+
+def remove_result_from_standings(standings_state, draft_pick_result):
+    for team, draft_pick in standings_state.items():
+        if draft_pick == draft_pick_result:
+            standings_state.pop(team)
+            break
+
+def get_team(standings_state, draft_pick_result):
+    for team, draft_pick in standings_state.items():
+        if draft_pick == draft_pick_result:
+            return team, draft_pick
+
+def do_lottery(standings):
+    draft_round = 14
+    standings_state = standings.copy()
+    results = {}
+
+    while draft_round > 3:
+        # when there is only 3 teams left, we apply the reaming teams in reverse standings order
+
+        draft_cohort = get_teams_for_lottery_round(standings_state)
+        draft_pick_result = draft_pick(draft_cohort, draft_round)
+
+        seed = nth_highest_seed(draft_cohort, draft_pick_result)
+        team, pick = get_team(standings_state, seed)
+
+        results[team] = pick
+        remove_result_from_standings(standings_state, pick)
+
+        draft_round -= 1
+
+    for x in range(3):
+        last_team = list(standings_state.items())[-1]
+        remove_result_from_standings(standings_state, last_team[1])
+        results[last_team[0]] = last_team[1]
+
+    return results
+
+def check_lottery(results):
+    for idx, result in enumerate(reversed(results)):
+        result_pick = 14 - idx
+        seed = 15-results[result]
+        seed_difference = seed - result_pick
+
+        if seed_difference > 3:
+            raise ValueError("Seed difference is greater than 3")
+        if seed_difference < -3:
+            raise ValueError("Seed difference is less than 3")
+
+        if seed_difference == 0:
+            seed_difference_string = "-"
+        else:
+            seed_difference_string = f"+{seed_difference}" if seed_difference > 0 else f"{seed_difference}"
+
+        print(f"{result_pick}: {result} {seed} ({seed_difference_string})")
+
+
+draft_round = 14
+
+with open("Standings.json", "r") as f:
+
+    standings = json.load(f)
+    res = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+        }
+
+    simulations = 1000000
+    print(f"Running {simulations} lottery simulation...")
+    for x in range(simulations):
+        y = lottery()
+        if y in res:
+            res[y] += 1
+        else:
+            res[y] = 1
+    for x in res:
+        print(f"{x}: {res[x]} ({res[x]/simulations*100}%)")
+
+    print("Running lottery...")
+    results = do_lottery(standings)
+
+    check_lottery(results)
+
+
+
+
+
+
+
